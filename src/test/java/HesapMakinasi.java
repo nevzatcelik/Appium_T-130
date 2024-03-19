43783e912a28a58d64307cf33d22c1c5cf5e288a
Nevzat <nevzatcelik151907@gmail.com>
2024-03-19 20:22:26 +0100

    second lesson

--- src/test/java/HesapMakinasi.java
+++ src/test/java/HesapMakinasi.java
@@ -6,6 +6,7 @@ import io.appium.java_client.ios.IOSDriver;
 import io.appium.java_client.ios.IOSElement;
 import io.appium.java_client.remote.MobileCapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
@@ -41,13 +42,21 @@ public class HesapMakinasi {
 
 
        // uygulamanin yuklendigini dogrular(isInstalled)
-
+        Assert.assertTrue(  driver.isAppInstalled("com.google.android.calculator"));
       // uygulamanin acildigini dogrular
-
+         Assert.assertTrue( driver.findElementById("com.google.android.calculator:id/clr").isDisplayed());
      // 400 un 3 katininin 1200 oldugunu hesap makinasindan dogrulayalim
+        driver.findElementByAccessibilityId("4").click();
+        driver.findElementByAccessibilityId("0").click();
+        driver.findElementByAccessibilityId("0").click();
 
+        driver.findElementByAccessibilityId("multiply").click();
 
+        driver.findElementByAccessibilityId("3").click();
 
+        String sonuc=driver.findElementById("com.google.android.calculator:id/result_preview").getText();
+        System.out.println(sonuc);
+        Assert.assertEquals(Integer.parseInt(sonuc),1200);
 
 
     }
